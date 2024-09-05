95c1d8e0a7f842a690ca0594733da7e6aaf712e6
kedabudre <abdurehimk77@gmail.com>
2024-09-05 04:03:24 +0200

    update the webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
+const webpack = require('webpack');
+const dotenv = require('dotenv');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const dotenv = require('dotenv');
 
 dotenv.config();
 
